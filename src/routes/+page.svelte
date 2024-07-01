d30d6c2c952e479a44eeaa72c11fbbc249ee8ba1
Evans Nightingale <lwhiteevans@gmail.com>
2024-07-01 22:59:51 +0700

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    function createTransitionInt(defaultValue: number) {
+        let value = $state(defaultValue);
+
+        function reset() {
+            const resetInterval = setInterval(
+                () => {
+                    if (value < defaultValue) {
+                        value++;
+                    } else if (value > defaultValue) {
+                        value--;
+                    } else {
+                        clearInterval(resetInterval);
+                    }
+                },
+                250 / Math.abs(value - defaultValue)
+            );
+        }
+
+        function limit() {
+            value = value !== Math.floor(value) ? ('' as unknown as number) : value;
+            value = value > 100 ? 100 : value;
+            value = value < 0 ? 0 : value;
+        }
+
+        function increment() {
+            value = value < 100 ? (value += 1) : value;
+        }
+
+        return {
+            get value() {
+                return value;
+            },
+            set value(newValue) {
+                value = newValue;
+            },
+            reset,
+            limit,
+            increment
+        };
+    }
+
+    const counter = createTransitionInt(0);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<button onclick={counter.increment}>Increment</button>
+<button onclick={counter.reset}>Reset</button>
+<input
+    type="range"
+    step={1}
+    min={0}
+    max={100}
+    bind:value={counter.value}
+    oninput={counter.limit}
+/>
+<input type="number" bind:value={counter.value} oninput={counter.limit} />
+<p>{counter.value}</p>
